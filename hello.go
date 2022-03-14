002445d0b73f09cb321b9d0d10a7d4c6ec4f2322
MigSanchez <24236168+MigSanchez@users.noreply.github.com>
2022-03-13 21:12:22 -0700

    hello tdd

--- hello.go
+++ hello.go
@@ -2,16 +2,37 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const french = "French"
+const china = "China"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+const chinaHelloPrefix = "Kneehow, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
 
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	case china:
+		prefix = chinaHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
