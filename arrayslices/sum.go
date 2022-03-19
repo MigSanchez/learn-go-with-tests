a4279bbddd71e73f6606eb060d418668489e572d
MigSanchez <24236168+MigSanchez@users.noreply.github.com>
2022-03-18 18:19:11 -0700

    arrays/slices

--- arrayslices/sum.go
+++ arrayslices/sum.go
@@ -7,3 +7,23 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(numbersToSun ...[]int) (sums []int) {
+	for _, numbers := range numbersToSun {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
+}
+
+func SumAllTails(numbersToSun ...[]int) (sums []int) {
+	for _, nums := range numbersToSun {
+		if len(nums) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := nums[1:]
+			sums = append(sums, Sum(tail))
+		}
+
+	}
+	return
+}
